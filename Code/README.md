7c232748715ce38e0992baea0edf4a3276264725
Karim Mohamed <abdolkarim.mohamed123@gmail.com>
2025-10-27 16:15:42 -0700

    delete
    delete

--- Code/README.md
+++
@@ -1,1 +0,0 @@
-This folder is for your project source code.
--- Code/app/src/main/java/com/example/eventdetails/Event.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eventdetails
-data class Event(
-    val name: String,
-    val time: String,
-    val location: String
-)
