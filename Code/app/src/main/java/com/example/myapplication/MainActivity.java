d43418b3380a86b25283b9924749384492c42dc8
Karim Mohamed <abdolkarim.mohamed123@gmail.com>
2025-10-27 16:16:28 -0700

    app
    app

---
+++ Code/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView helloText;
+    private Button clickButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helloText = findViewById(R.id.helloText);
+        clickButton = findViewById(R.id.clickButton);
+
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helloText.setText("Button Clicked!");
+            }
+        });
+    }
+}
